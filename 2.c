29fce852323843bbb138e145294e1050b1bdc2a1
explorer02 <46781998+explorer02@users.noreply.github.com>
2019-09-30 13:29:51 +0530

    Add files via upload
    this is a sample c file.

---
+++ 2.c
@@ -0,0 +1,11 @@
+//Program to print ASCII code of an input key stroke.
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter any character to find it's equivalent ASCII code  ");
+    scanf("%c",&ch);
+    int c=ch;
+    printf("Equivalent ASCII code: %d",c);
+    return 0;
+}
